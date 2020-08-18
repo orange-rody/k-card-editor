b63f1d15d84420ec94233aa22b44f77a3349b27d
hiro_from_22 <ore097miki@gmail.com>
2020-08-18 20:19:27 +0900

    カルーセルの実装がどうしてもできませんでした

--- js/cardViewerTest1.js
+++ js/cardViewerTest1.js
@@ -30,21 +30,21 @@ auth.onAuthStateChanged(user => {
         let size = snapshot.size;
         size = size.toString();
         size = parseInt(size);
-        let postedCardNumber = document.querySelector('#postedCardNumber');
-        postedCardNumber.textContent = size;
-        return postedCardNumber;
+        console.log(size);
+        return size;
       }
-      sizetoNumber();
+
+      // postedCardNumberにuid === currentUidの要素数を代入する。
+      let postedCardNumber = document.querySelector('#postedCardNumber');
+      postedCardNumber.textContent = sizetoNumber();
+
+      
+      // let cardSlides = document.createElement('div');
+      // cardSlides.setAttribute('id','cardSlides');
+      // cardSlides.style.height = `calc(100vh * ${sizetoNumber()})`;
+      
+
       snapshot.docs.forEach((doc)=>{
-        function sizetoNumber(){
-          // sizeプロパティで要素の数を取得 ※クライアント側でダウンロードしてカウントするため、 100程度のコレクションでしか使えない。
-          let size = snapshot.size;
-          size = size.toString();
-          size = parseInt(size);
-          let postedCardNumber = document.querySelector('#postedCardNumber');
-          postedCardNumber.textContent = size;
-          return postedCardNumber;
-        }
         renderCard(doc);
       });
     });
@@ -62,14 +62,14 @@ auth.onAuthStateChanged(user => {
       profileSentence.textContent = doc.data().profile;
       favorite.insertAdjacentHTML('beforeend',doc.data().favorite);
     }
+  
+
     function renderCard(doc){
-
-      let mainCenter = document.querySelector('#main-center');
-
       //カードを「戻る」ボタンを作るための要素を追加
-      let carouselControlPrev = document.createElement('a');
+      // let carouselControlPrev = document.createElement('a');
       //カードを「進める」ボタンを作るための要素を追加
-      let carouselControlNext = document.createElement('a');
+      // let carouselControlNext = document.createElement('a');
+      let mainCenter = document.querySelector('#main-center');
 
       let cardContainer = document.createElement('div');
       let cardWrap = document.createElement('div');
@@ -119,11 +119,10 @@ auth.onAuthStateChanged(user => {
       postedUserIcon.style.backgroundImage = "url(https://firebasestorage.googleapis.com/v0/b/k-card-editor.appspot.com/o/Hiroki%2FIMG_0117.JPG?alt=media&token=4925a255-05d4-4c01-b3c4-d36073e8149b)";
       
       //カルーセル「戻る」ボタン要素を作るために、fontawesomeのアイコンをhtmlに追加
-      carouselControlPrev.innerHTML = '<i class="far fa-caret-square-up"></i>';
+      // carouselControlPrev.innerHTML = '<i class="far fa-caret-square-up"></i>';
       //カルーセル「進める」ボタン要素を作るために、fontawesomeのアイコンをhtmlに追加
-      carouselControlNext.innerHTML = '<i class="far fa-caret-square-down"></i>';
+      // carouselControlNext.innerHTML = '<i class="far fa-caret-square-down"></i>';
 
-   
       cardContainer.setAttribute('id','cardContainer');
       cardWrap.setAttribute('id','cardWrap');
       cardViewer.setAttribute('id','cardViewer');
@@ -147,15 +146,14 @@ auth.onAuthStateChanged(user => {
       postedUserIcon.setAttribute('id','postedUserIcon');
     
       //カルーセル「戻る」ボタンにid = carouselControlPrev を追加
-      carouselControlPrev.setAttribute('id','carouselControlPrev');
+      // carouselControlPrev.setAttribute('id','carouselControlPrev');
       //カルーセル「進める」ボタンにid = carouselControlNext を追加
-      carouselControlNext.setAttribute('id','carouselControlNext');
+      // carouselControlNext.setAttribute('id','carouselControlNext');
 
       //カルーセル「戻る」ボタンにclass = carousel を追加
-      carouselControlPrev.setAttribute('class','carouselControl');
+      // carouselControlPrev.setAttribute('class','carouselControl');
       //カルーセル「進める」ボタンにclass = carousel を追加
-      carouselControlNext.setAttribute('class','carouselControl');
-
+      // carouselControlNext.setAttribute('class','carouselControl');
 
       let editorURL = `k-card-editor.html?doc.id=${doc.id}`;
       revisionButton.setAttribute('href',editorURL);
@@ -181,65 +179,67 @@ auth.onAuthStateChanged(user => {
       cardStatus.appendChild(postedUserIcon);
       cardWrap.appendChild(cardStatus);
       cardContainer.appendChild(cardWrap);
-
       mainCenter.appendChild(cardContainer);
 
       //mainCenterにカルーセル「戻る」ボタンであるcarouselCotrolPrevを追加
-      mainCenter.appendChild(carouselControlPrev);
+      // mainCenter.appendChild(carouselControlPrev);
       //mainCenterにカルーセル「進める」ボタンであるcarouselControlNextを追加
-      mainCenter.appendChild(carouselControlNext);
-
-      //カードコンテナ1枚あたりの高さを設定(vh)
-      const cardContainerHeight = 100;
-
-      //現在表示されているカードコンテナが何枚目か(0から数え始める)
-      let currentCardContainer = 0;
-
-      //カードコンテナの全枚数
-      let numCardContainers = postedCardNumber;
-
-      //index番目(0から始まる)のカードコンテナを表示する関数
-      const showCardContainer = (index) => {
-        //1番目のカードコンテナでは上矢印を非表示にする
-        if(index === 0){
-          $('#carouselControlPrev').hide();
-        }else {
-          $('#carouselControlNext').show();
-        }
-        if(index === numCardContainers - 1){
-          $('#carouselControlPrev').show();
-        }else {
-          $('#carouselControlNext').hide();
-        }
-        // 実行中のアニメーションがあればキャンセルした後、
-        // topを変化させるアニメーションを開始
-        $('#main-center')
-        .stop()
-        .animate(
-          {
-            top:`${cardContainerHeight * index}vh`
-          },600
-        );
-      }
-
-        //上矢印がクリックされたら、一つ前のカードコンテナを表示
-        $('#carouselControlPrev').on('click',(e)=>{
-          e.preventDefault();
-          currentCardContainer -= 1;
-          showCardContainer(currentCardContainer);
-        })
-
-        //下矢印がクリックされたら、一つ後のカードコンテナを表示
-        $('#carouselControlNext').on('click',(e)=>{
-          e.preventDefault();
-          currentCardContainer += 1;
-          showCardContainer(currentCardContainer);
-        })
-
-        showCardContainer(currentCardContainer);
+      // mainCenter.appendChild(carouselControlNext);
 
     }
       
+     //カードコンテナ1枚あたりの高さを設定(vh)
+     const cardContainerHeight = 100;
+
+     //現在表示されているカードコンテナが何枚目か(0から数え始める)
+    let currentCardContainer = 100;
+
+     //カードコンテナの全枚数
+     let numCardContainers = sizetoNumber();
+
+     //index番目(0から始まる)のカードコンテナを表示する関数
+     const showCardContainer = (index) => {
+       console.log(index);
+       //1番目のカードコンテナでは上矢印を非表示にする
+      //  if(index === 0){
+      //    $('#carouselControlPrev').hide();
+      //  }else {
+      //    $('#carouselControlNext').show();
+      //  }
+      //  if(index === numCardContainers - 1){
+      //    $('#carouselControlPrev').show();
+      //  }else {
+      //    $('#carouselControlNext').hide();
+      //  }
+       // 実行中のアニメーションがあればキャンセルした後、
+       // topを変化させるアニメーションを開始
+       $('#main-center')
+       .stop()
+       .animate(
+         {
+           top:`${cardContainerHeight * index}vh`
+         },600
+       );
+     }
+
+      window.addEventListener('scroll',(e)=>{
+        e.preventDefault();
+           currentCardContainer -= 1;
+           showCardContainer(currentCardContainer);
+      });
+      
+      //    
+      //  })
+
+       //下矢印がクリックされたら、一つ後のカードコンテナを表示
+      window.addEventListener('scroll',(e)=>{
+        e.preventDefault();
+           currentCardContainer += 1;
+           showCardContainer(currentCardContainer);
+      });
+   
+
+       showCardContainer(currentCardContainer);
     
 
     db.collection("k-card").onSnapshot((snapshot)=>{
