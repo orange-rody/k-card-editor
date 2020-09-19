525964273257e193e6a31aca3e677c8860cc382c
hiro_from_22 <ore097miki@gmail.com>
2020-09-19 17:51:04 +0900

    ユーザープロフィール画面における２つの画像入力フォームのうち、片方しか入力していない場合でも結果が反映されるようにしました。

--- js/profileEdit.js
+++ js/profileEdit.js
@@ -35,24 +35,69 @@ auth.onAuthStateChanged((user) => {
       profile.innerHTML = `${user.data().profile}`;
       favorite.innerHTML = `${user.data().favorite}`;
     });
+
     form.addEventListener('submit',(e)=>{
       e.preventDefault();
       const userIconFiles = document.getElementById('upload-userIcon').files;
       const userImageFiles = document.getElementById('upload-userImage').files;
       if(userIconFiles.length===0){
-        //ファイルが選択されていないなら何もしない
+
+        const userImageFile = userImageFiles[0];
+        //userImageファイルのファイル名を取得する 例：dog.jpg  dog.png
+        const userImageFileName = userImageFile.name;
+        //split()を使って'.'を区切りとして文字列を分割。pop()で末尾の要素(jpg,pngなどの拡張子)を切り取る。
+        const userImageFileType = userImageFileName.split('.').pop();
+        console.log(userImageFileType);
+        //Firebase Storageには「userIcon.拡張子」の名称で画像データが保存されるようにする。
+        storage.ref().child(`${currentUid}/userImage.${userImageFileType}`)
+        //userImageFileは元のファイル名のままでOK。
+                 .put(userImageFile)
+                 //画像ファイルをStorageの指定の場所にアップロード。
+                 .then(()=>{
+                    console.log(userImageFile);
+                    location.href = 'cardViewerTest1.html';
+                  });
+        
         return;
       } 
       else if(userImageFiles.length===0){
-        return;
+      
+        const userIconFile = userIconFiles[0];
+        //userIconファイルのファイル名を取得する 例：dog.jpg  dog.png
+        const userIconFileName = userIconFile.name;
+        //split()を使って'.'を区切りとして文字列を分割。pop()で末尾の要素(jpg,pngなどの拡張子)を切り取る。
+        const userIconFileType = userIconFileName.split('.').pop();
+        console.log(userIconFileType);
+        //Firebase Storageには「userIcon.拡張子」の名称で画像データが保存されるようにする。
+        storage.ref().child(`${currentUid}/userIcon.${userIconFileType}`)
+        //userIconFileは元のファイル名のままでOK。
+               .put(userIconFile)
+                 //storageへの登録は一番時間がかかる処理なので、それが完了してから
+                 //cardViewerTest1.htmlに戻るようにする。※でないと、画像登録できないままになってしまう
+                 //currentUidしかデータを編集できないようにするため、一番上位の親フォルダにcurrentUidフォルダを作成する。
+                 //画像ファイルに個人名をつけるユーザーも想定されることから、匿名性を保つ必要がある。したがって、登録する画像のタイトルを固定し、拡張子のみ反映されるようにする。
+               .then(()=>{
+                 location.href = "cardViewerTest1.html";
+               });
       }
+
       const userIconFile = userIconFiles[0];
       //userIconファイルのファイル名を取得する 例：dog.jpg  dog.png
       const userIconFileName = userIconFile.name;
       //split()を使って'.'を区切りとして文字列を分割。pop()で末尾の要素(jpg,pngなどの拡張子)を切り取る。
       const userIconFileType = userIconFileName.split('.').pop();
       console.log(userIconFileType);
-      
+      //Firebase Storageには「userIcon.拡張子」の名称で画像データが保存されるようにする。
+      storage.ref().child(`${currentUid}/userIcon.${userIconFileType}`)
+      //userIconFileは元のファイル名のままでOK。
+             .put(userIconFile)
+               //storageへの登録は一番時間がかかる処理なので、それが完了してから
+               //cardViewerTest1.htmlに戻るようにする。※でないと、画像登録できないままになってしまう
+               //currentUidしかデータを編集できないようにするため、一番上位の親フォルダにcurrentUidフォルダを作成する。
+               //画像ファイルに個人名をつけるユーザーも想定されることから、匿名性を保つ必要がある。したがって、登録する画像のタイトルを固定し、拡張子のみ反映されるようにする。
+             .then(()=>{
+               location.href = "cardViewerTest1.html";
+             });
 
       const userImageFile = userImageFiles[0];
       //userImageファイルのファイル名を取得する 例：dog.jpg  dog.png
@@ -60,17 +105,7 @@ auth.onAuthStateChanged((user) => {
       //split()を使って'.'を区切りとして文字列を分割。pop()で末尾の要素(jpg,pngなどの拡張子)を切り取る。
       const userImageFileType = userImageFileName.split('.').pop();
       console.log(userImageFileType);
-     
       //Firebase Storageには「userIcon.拡張子」の名称で画像データが保存されるようにする。
-      storage.ref().child(`${currentUid}/userIcon.${userIconFileType}`)
-      //userIconFileは元のファイル名のままでOK。
-             .put(userIconFile);
-               //storageへの登録は一番時間がかかる処理なので、それが完了してから
-               //cardViewerTest1.htmlに戻るようにする。※でないと、画像登録できないままになってしまう
-               //currentUidしかデータを編集できないようにするため、一番上位の親フォルダにcurrentUidフォルダを作成する。
-               //画像ファイルに個人名をつけるユーザーも想定されることから、匿名性を保つ必要がある。したがって、登録する画像のタイトルを固定し、拡張子のみ反映されるようにする。
-
-       //Firebase Storageには「userIcon.拡張子」の名称で画像データが保存されるようにする。
       storage.ref().child(`${currentUid}/userImage.${userImageFileType}`)
       //userImageFileは元のファイル名のままでOK。
                .put(userImageFile)
