27fe81763a33ff82954831abb06bdeb05d395094
hiro_from_22 <ore097miki@gmail.com>
2020-09-20 02:28:37 +0900

    jpgとpngの両方のデータを登録できるようにしました。

--- js/profileEdit.js
+++ js/profileEdit.js
@@ -12,22 +12,11 @@ const userIconZone = document.querySelector('.userIcon-zone');
 const userImageZone = document.querySelector('.userImage-zone');
 
 
+
+
 auth.onAuthStateChanged((user) => {
   if(user){
     let currentUid = user.uid;
-    // storage.ref(`${currentUid}/userIcon`)
-    // .getDownloadURL()
-    // .then((url)=>{
-    //   userIconZone.style.backgroundImage = `url(${url})`;
-    // });
-
-    // storage.ref(`${currentUid}/userImage`)
-    // .getDownloadURL()
-    // .then((url)=>{
-    //   userImageZone.style.backgroundImage = `url(${url})`;
-    // })
-
-
 
     db.collection('user').doc(currentUid).get()
     .then((user)=>{
@@ -35,6 +24,7 @@ auth.onAuthStateChanged((user) => {
       profile.innerHTML = `${user.data().profile}`;
       favorite.innerHTML = `${user.data().favorite}`;
     });
+
 
     function putUserIcon(){
       const userIconFiles = document.getElementById('upload-userIcon').files;
@@ -44,20 +34,33 @@ auth.onAuthStateChanged((user) => {
       //split()を使って'.'を区切りとして文字列を分割。pop()で末尾の要素(jpg,pngなどの拡張子)を切り取る。
       const userIconFileType = userIconFileName.split('.').pop();
       console.log(userIconFileType);
-      //Firebase Storageには「userIcon.拡張子」の名称で画像データが保存されるようにする。
-      storage.ref().child(`${currentUid}/userIcon.${userIconFileType}`)
-      //userIconFileは元のファイル名のままでOK。
-            .put(userIconFile)
-              //storageへの登録は一番時間がかかる処理なので、それが完了してから
-              //cardViewerTest1.htmlに戻るようにする。※でないと、画像登録できないままになってしまう
-              //currentUidしかデータを編集できないようにするため、一番上位の親フォルダにcurrentUidフォルダを作成する。
-              //画像ファイルに個人名をつけるユーザーも想定されることから、匿名性を保つ必要がある。したがって、登録する画像のタイトルを固定し、拡張子のみ反映されるようにする。
-            .then(()=>{
-              location.href = "cardViewerTest1.html";
-           });
+      //userIconFileType === jpg か、userIconFileType === pngで場合分け
+      if(userIconFileType === 'jpg'){
+        storage.ref().child(`${currentUid}/userIcon.png`).delete();
+        //Storageに「userIcon.jpg」の名称で画像データを保存。
+        storage.ref().child(`${currentUid}/userIcon.jpg`)
+        //userImageFileは元のファイル名のままでOK。
+               .put(userIconFile)
+               //画像ファイルをStorageの指定の場所にアップロード。
+               //storageへの登録は一番時間がかかる処理なので、それが完了してから
+               //cardViewerTest1.htmlに戻るようにする。※でないと、画像登録できないままになってしまう
+               //currentUidしかデータを編集できないようにするため、一番上位の親フォルダにcurrentUidフォルダを作成する。
+               //画像ファイルに個人名をつけるユーザーも想定されることから、匿名性を保つ必要がある。したがって、登録する画像のタイトルを固定し、拡張子のみ反映されるようにする。
+               .then(()=>{
+                  console.log('アイコン画像の登録処理が完了しました');
+              });
+      }else{
+        storage.ref().child(`${currentUid}/userIcon.jpg`).delete();
+        //Storageに「userIcon.png」の名称で画像データを保存。
+        storage.ref().child(`${currentUid}/userIcon.png`)
+               .put(userIconFile)
+               .then(()=>{
+                  console.log('アイコン画像の登録処理が完了しました');
+              });
+      }  
     }
 
-    function putUserImage(){
+    function putUserImage(){  
       const userImageFiles = document.getElementById('upload-userImage').files;
       const userImageFile = userImageFiles[0];
       //userImageファイルのファイル名を取得する 例：dog.jpg  dog.png
@@ -68,35 +71,24 @@ auth.onAuthStateChanged((user) => {
       //userImageFileType === jpg か、userImageFileType === pngで場合分け
       if(userImageFileType === 'jpg'){
         storage.ref().child(`${currentUid}/userImage.png`).delete();
-        //Firebase Storageに「userIcon.jpg」の名称で画像データを保存。
-        storage.ref().child(`${currentUid}/userImage.`)
+        //Storageに「userIcon.jpg」の名称で画像データを保存。
+        storage.ref().child(`${currentUid}/userImage.jpg`)
         //userImageFileは元のファイル名のままでOK。
                .put(userImageFile)
                //画像ファイルをStorageの指定の場所にアップロード。
                .then(()=>{
-                  console.log(userImageFile);
-                  location.href = 'cardViewerTest1.html';
-              });
+                console.log('イメージ画像の登録処理が完了しました');
+            });
       }else{
-
+        storage.ref().child(`${currentUid}/userImage.jpg`).delete();
+        //Storageに「userIcon.png」の名称で画像データを保存。
+        storage.ref().child(`${currentUid}/userImage.png`)
+               .put(userImageFile)
+               .then(()=>{
+                  console.log('イメージ画像の登録処理が完了しました');
+              });
       }
     }              
-
-    
-    function deleteUserImage(){
-      storage.ref().child(`${currentUid}/userIcon.jpg`).getDownloadURL().then(onResolveIcon, onRejectIcon);
-      
-      function onResolveIcon(){
-        storage.ref().child(`${currentUid}/userIcon.jpg`).delete();
-      }
-      
-      function onRejectIcon(){
-        storage.ref().child(`${currentUid}/userIcon.png`).delete();
-      }
-    }
-
-    
-     
 
     form.addEventListener('submit',(e)=>{
       e.preventDefault();
@@ -113,17 +105,27 @@ auth.onAuthStateChanged((user) => {
         setTimeout(function(){location.href='cardViewerTest1.html'},500);
       }
       else if(userIconFiles.length===0){
-        deleteUserImage();
-        setTimeout(putUserImage(),1500);
+        putUserImage();
+        setTimeout(function(){
+          console.log('ユーザーアイコン登録の処理が完了しました');
+          location.href = 'cardViewerTest1.html';
+          },1000
+        );
       } 
       else if(userImageFiles.length===0){
-        deleteUserIcon();
-        setTimeout(putUserIcon(),1500); 
+        putUserIcon();
+        setTimeout(function(){
+          console.log('ユーザーイメージ登録の処理が完了しました');
+          location.href = 'cardViewerTest1.html';
+          },1000
+        );
       }
-     deleteUserImage();
-     setTimeout(putUserImage(),1500);
-     deleteUserIcon();
-     setTimeout(putUserIcon(),1500);
+       putUserIcon();
+       putUserImage();
+       setTimeout(function(){
+         console.log('全ての処理が完了しました');
+         location.href = 'cardViewerTest1.html';
+       },1000);
     });
-  }
-});
+    }
+  });
